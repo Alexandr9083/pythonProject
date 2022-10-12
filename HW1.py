bb7f532527477035a49fc0ee5481aab72267d777
Alexandr <blackvice@yandex.ru>
2022-10-12 18:52:44 +0300

    commit2

--- HW1.py
+++ HW1.py
@@ -4,11 +4,21 @@
 # - 7 -> да
 # - 1 -> нет
 
-number = int(input("Введите число дня недели от 1 до 7: "))
+# number = int(input("Введите число дня недели от 1 до 7: "))
+#
+# if number < 1 or number > 7:
+#     print('Вы ввели неверное число! До встречи ;)')
+# elif number > 5:
+#     print('Ура, этот день выходной!')
+# else:
+#     print('Увы, но это рабочий день!')
 
-if number < 1 or number > 7:
-    print('Вы ввели неверное число! До встречи ;)')
-elif number > 5:
-    print('Ура, этот день выходной!')
-else:
-    print('Увы, но это рабочий день!')
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# if not (x or y or z) == (not x and not y and not z):
+
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             if not (x or y or z) == (not x and not y and not z):
+#                 print(f'Утверждение ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z истенно при x = {x}, y={y}, z={z}')
+
